cf81577a33b4a30acc8ee77da7cb5b8bd7538911
Oscar Petersen <oscarpetersen7@gmail.com>
2024-07-04 16:52:12 +0100

    better figure

--- PLOTTING/dataplotting.py
+++ PLOTTING/dataplotting.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 def plot_means(data, signals):
     if 'med_ema' in signals:
+        plt.figure(figsize=(16,8))
         plt.plot(data['Close'], label = 'Close Price', color = 'black')
         plt.plot(signals['short_ema'], label = 'Short Exponential Moving Average', color = 'red')
         plt.plot(signals['med_ema'], label = 'Medium Exponential Moving Average', color = 'blue')
@@ -13,6 +14,7 @@ def plot_means(data, signals):
         plt.grid(True)
         plt.show()
     else:
+        plt.figure(figsize=(16,8))
         plt.plot(data['Close'], label = 'Close Price', color = 'blue')
         plt.plot(signals['short_mavg'], label = 'Short Moving Average', color = 'red')
         plt.plot(signals['long_mavg'], label = 'Long Moving Average', color = 'green')
